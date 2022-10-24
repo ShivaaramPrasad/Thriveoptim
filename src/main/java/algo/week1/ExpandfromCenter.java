035f69cad8d26937a41c57e6900b2eb9cdef63fa
ShivaaramPrasad <shivaa.ram25@gmail.comgit config --global user.email shivaa.ram25@gmail.comgit config --global user.email shivaa.ram25@gmail.com>
2022-10-24 09:16:46 +0530

    vowel problem completed

---
+++ src/main/java/algo/week1/ExpandfromCenter.java
@@ -0,0 +1,21 @@
+package algo.week1;
+
+public class ExpandfromCenter {
+	
+	/*
+	 ODD
+	 Length 5
+	 length n/2 2
+	 left = length/2 - length%2  2-1=1
+	 right = length/2 + length%2  2+1=3
+	 
+	 
+	 Even 
+	 Length 4
+	 length n/2 2
+	 left = length/2 - length%2  2-0=2
+	 left = length/2 + length%2  2-0=2 
+
+	 */
+
+}
