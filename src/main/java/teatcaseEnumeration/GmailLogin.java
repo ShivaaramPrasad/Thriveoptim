f06e87b623efb78029159df54332b0282fef9674
ShivaaramPrasad <shivaa.ram25@gmail.com>
2023-03-01 22:50:23 +0530

    tc gmaillogin

---
+++ src/main/java/teatcaseEnumeration/GmailLogin.java
@@ -0,0 +1,44 @@
+package teatcaseEnumeration;
+
+public class GmailLogin {
+	
+	/*
+	 - UI
+	 - Use Valid username and password 
+	 - Use Invalid username and valid password
+	 - Use Valid username and invalid password
+	 - Use Empty username and password
+	 - Use  username and blank password
+	 - Use Blank username and valid password:
+	 - Use Incorrect username format and Incorrect password format
+	 - blocked user should not allow
+	 - while doing forgot password if i try to login 
+     - Check cross  browser 
+    - Scanner email link notification
+    -  if the login done in Untrust device it should prompt the user to do rest  password 
+    - two factor acuthenication we need to check 
+   - if sso login compatable other other website
+- API
+   - Check function testing in api  
+   - login with sso 
+   - check bearer token authentication 
+   - password is encrypted or not
+   - request and response and response code 
+   
+- Compatibility -  - diff platforms (web/ app/ tablet/ IOT)
+
+- Security -  bearer token authentication 
+            - Memory leaks
+            - logout is happening in all devices  if change the password 
+- Performance 
+   - load sequentially 
+   - spike test 
+   - concurrent
+   -Scale (horizontal & vertical)
+
+Localization / globalization - Mutiple lanuage support 
+	
+*/
+
+
+}
